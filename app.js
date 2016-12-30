6a4f5338b90e41c5089ec37414fc06501214b28b
t-kusakabe <ttmac0704@gmail.com>
2016-12-30 11:35:47 +0900

    business logic done

--- app.js
+++ app.js
@@ -46,6 +46,24 @@ angular.module('App', []).controller('MainController', ['$scope', '$filter', fun
     }
     $scope.editing = originalTitle = null;
   };
+
+  $scope.checkAll = function () {
+    var state = !!$scope.remainingCount;
+
+    angular.forEach($scope.todos, function (todo) {
+      todo.done = state;
+    });
+  };
+
+  $scope.removeDoneTodo = function () {
+    $scope.todos = where($scope.todos, $scope.filter.remaining);
+  };
+
+  $scope.removeTodo = function (currentTodo) {
+    $scope.todos = where($scope.todos, function (todo) {
+      return currentTodo !== todo;
+    });
+  };
 }])
 .directive('mySelect', [function () {
   return function (scope, $el, attrs) {
